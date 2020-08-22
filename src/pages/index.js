eabb69db6da76811ab61498b073b96acc1e26780
Gabriel Brun <lydstyl@gmail.com>
2020-08-23 00:32:34 +0200

    html-react-parser

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
 import React from "react"
+import { graphql } from "gatsby"
+import parse from "html-react-parser"
+import Img from "gatsby-image"
 
 import Layout from "../components/Layout/Layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+// import Image from "../components/image"
 
-const IndexPage = ({
-  data: {
-    datoCmsHomePage: { title },
-  },
-}) => {
+const IndexPage = ({ data: { datoCmsHomePage } }) => {
+  const {
+    title,
+    image,
+    introNode: {
+      childMarkdownRemark: { html },
+    },
+  } = datoCmsHomePage
+
   return (
     <Layout>
-      <SEO title="Bienvenue" />
+      <SEO title={title} />
       <h1>{title}</h1>
-      <p>
-        Ullamco elit sunt elit ex elit adipisicing consectetur laboris labore.
-      </p>
-      <p>In cillum aute officia commodo eiusmod nulla.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+
+      <div className="intro">{parse(html)}</div>
+
+      {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
-      </div>
+      </div> */}
+      <article>
+        <Img fluid={image.fluid} />
+      </article>
     </Layout>
   )
 }
@@ -28,6 +37,16 @@ export const query = graphql`
   query HomeQuery {
     datoCmsHomePage {
       title
+      image {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+      introNode {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
   }
 `
