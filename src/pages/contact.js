e1bc6343640e2b1876726aea2187c469f14b193f
Gabriel Brun <lydstyl@gmail.com>
2020-08-23 01:43:54 +0200

    dato cms connected with origin job & contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
+import parse from "html-react-parser"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
 
-const Contact = () => (
+const Contact = ({
+  data: {
+    datoCmsContact: { contact },
+  },
+}) => (
   <Layout>
     <SEO title="Contact" />
+
     <h1>Contact</h1>
-    <p>
-      Ex dolor labore exercitation Lorem laboris enim veniam incididunt
-      incididunt laborum ea cupidatat reprehenderit ullamco. Cupidatat veniam
-      reprehenderit non quis laboris et excepteur ipsum ipsum culpa qui magna.
-      Ullamco laborum occaecat fugiat incididunt Lorem deserunt magna eiusmod eu
-      pariatur ea. Minim do id reprehenderit nostrud sit eu nulla eu occaecat
-      amet velit tempor sunt veniam. Cillum consequat sit cupidatat quis mollit
-      voluptate ut consequat velit enim duis ipsum. Aute Lorem ea et amet nulla
-      occaecat ullamco anim quis. Cupidatat dolor aliquip culpa nisi
-      reprehenderit occaecat fugiat et pariatur.
-    </p>
+
+    <div className="content">{parse(contact)}</div>
 
     <Link to="/">Revenir à la page d'acceuil</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    datoCmsContact {
+      contact
+    }
+  }
+`
 
 export default Contact
--- src/pages/metier.js
+++ src/pages/metier.js
@@ -1,32 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
+import parse from "html-react-parser"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
 
-const Metier = () => (
+const Metier = ({
+  data: {
+    datoCmsJob: { job },
+  },
+}) => (
   <Layout>
     <SEO title="Mon métier" />
-    <h1>Mon métier</h1>
-    <p>
-      Proident occaecat anim commodo non ad ex. Anim eu qui minim nulla occaecat
-      velit deserunt. Enim nisi do id eu enim elit cillum officia. Pariatur ex
-      eu adipisicing labore mollit cillum culpa ea eiusmod. Velit cupidatat
-      adipisicing ex amet officia exercitation ea nostrud.
-    </p>
 
-    <p>
-      Occaecat Lorem dolor incididunt ea nulla deserunt quis. Voluptate irure
-      veniam cillum minim mollit ex cupidatat irure excepteur aliqua consectetur
-      anim mollit nulla. Irure minim aliquip in consectetur officia proident
-      enim nisi do tempor. In velit aliquip reprehenderit eiusmod amet. Veniam
-      consectetur aute nulla tempor nostrud ea duis veniam aliquip veniam.
-      Aliquip cillum sit qui in officia amet laboris. Esse elit ut est pariatur
-      magna ut magna cillum aute eiusmod ut.
-    </p>
+    <h1>Mon métier</h1>
+
+    <div className="content">{parse(job)}</div>
 
     <Link to="/">Revenir à la page d'acceuil</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    datoCmsJob {
+      job
+    }
+  }
+`
 
 export default Metier
